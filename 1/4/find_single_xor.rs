347498e2de06cdc55ac7db46dcf4d1c2a5497295
Dmitry Vasiliev <dima@hlabs.org>
2014-01-07 21:40:42 +0100

    First draft of single-character XOR finder

---
+++ 1/4/find_single_xor.rs
@@ -0,0 +1,41 @@
+// Find a string encrypted with single-character XOR cipher
+//
+// Dmitry Vasiliev <dima@hlabs.org>
+//
+
+extern mod single_xor_lib;
+
+extern mod extra;
+
+use std::path::Path;
+use std::io::fs::File;
+use std::io::buffered::BufferedReader;
+use extra::hex::FromHex;
+use single_xor_lib::{decrypt, Found, NotFound};
+
+fn find_encrypted(file: File) {
+    use std::str;
+
+    let mut reader = BufferedReader::new(file);
+    for line in reader.lines() {
+        // TODO: Skip invalid hex encoded lines
+        let encrypted = line.from_hex().unwrap();
+        match decrypt(encrypted) {
+            Found(key, decrypted) => {
+                    println!("Key        => {}", key);
+                    println!("Decrypted  => {}", str::from_utf8(decrypted));
+                    //println!("Decrypted  => {:?}", decrypted);
+                }
+            NotFound => ()
+        }
+    }
+}
+
+#[cfg(not(test))]
+fn main() {
+    let path = Path::new("strings.txt");
+    match File::open(&path) {
+        Some(file) => find_encrypted(file),
+        None => fail!("Unable to open strings.txt")
+    }
+}

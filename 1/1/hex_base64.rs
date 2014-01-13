40fa028be2bb5a9f4ac0b657a906e91d885c9910
Dmitry Vasiliev <dima@hlabs.org>
2014-01-13 19:46:20 +0100

    Clean up hex-base64 converter

--- 1/1/hex_base64.rs
+++ 1/1/hex_base64.rs
@@ -1,7 +1,8 @@
-// Converting between Hex and Base64 encodings
-//
-// Dmitry Vasiliev <dima@hlabs.org>
-//
+/* Converting between Hex and Base64 encodings
+ *
+ * Dmitry Vasiliev <dima@hlabs.org>
+ */
+
 
 extern mod extra;
 
@@ -31,6 +32,9 @@ fn bytes_to_hex(bytes: &[u8]) -> ~str {
     bytes.to_hex()
 }
 
+/*
+ * Main entry point
+ */
 #[cfg(not(test))]
 fn main() {
     use std::str;
@@ -48,12 +52,21 @@ fn main() {
     println!("Hex 2   => {}", hex2);
 }
 
-#[test]
-fn test_base64_hex() {
-    let hex = ~"49276d206b696c6c696e6720796f757220627261696e206c696b65206120\
-                706f69736f6e6f7573206d757368726f6f6d";
-    let base64 = ~"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3V\
-                   zIG11c2hyb29t";
-    assert_eq!(bytes_to_base64(hex_to_bytes(hex)), base64);
-    assert_eq!(bytes_to_hex(base64_to_bytes(base64)), hex);
+/*
+ * Tests
+ */
+#[cfg(test)]
+mod test {
+    use super::{bytes_to_base64, base64_to_bytes};
+    use super::{hex_to_bytes, bytes_to_hex};
+
+    #[test]
+    fn test_base64_hex() {
+        let hex = ~"49276d206b696c6c696e6720796f757220627261696e206c696b65\
+                    206120706f69736f6e6f7573206d757368726f6f6d";
+        let base64 = ~"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3V\
+                       zIG11c2hyb29t";
+        assert_eq!(bytes_to_base64(hex_to_bytes(hex)), base64);
+        assert_eq!(bytes_to_hex(base64_to_bytes(base64)), hex);
+    }
 }

6261a331acaaa45c5c0fe4bf3f724d938a2daafc
Dmitry Vasiliev <dima@hlabs.org>
2014-01-13 21:54:26 +0100

    Clean up code, add tests

--- 1/3/single_xor.rs
+++ 1/3/single_xor.rs
@@ -1,19 +1,21 @@
-// Single-character XOR cipher guesser
-//
-// Dmitry Vasiliev <dima@hlabs.org>
-//
+/* Single-character XOR cipher guesser
+ *
+ * Dmitry Vasiliev <dima@hlabs.org>
+ */
 
 extern mod single_xor_lib;
 
 extern mod extra;
 
+use std::str;
+
 use single_xor_lib::{decrypt, Found, NotFound};
 use extra::hex::FromHex;
 
-#[cfg(not(test))]
+/*
+ * Main entry point
+ */
 fn main() {
-    use std::str;
-
     let input = ~"1b37373331363f78151b7f2b783431333d78397828372d363c\
                   78373e783a393b3736";
     let encrypted = input.from_hex().unwrap();
@@ -22,7 +24,7 @@ fn main() {
     println!("Encrypted  => {:?}", encrypted);
     match decrypt(encrypted) {
         Found(key, decrypted) => {
-                println!("Key        => {}", key);
+                println!("Key        => {}, {}", key, key as char);
                 println!("Decrypted  => {}", str::from_utf8(decrypted));
             }
         NotFound => fail!("No decryption key found")
--- 1/3/single_xor_lib.rs
+++ 1/3/single_xor_lib.rs
@@ -1,16 +1,16 @@
-// Single-character XOR cipher guesser library
-//
-// Dmitry Vasiliev <dima@hlabs.org>
-//
-
-// TODO:
-//  - Tests
+/* Single-character XOR cipher guesser library
+ *
+ * Dmitry Vasiliev <dima@hlabs.org>
+ */
 
 #[crate_id="single_xor_lib#0.1"];
 #[crate_type="lib"];
 
+extern mod extra;
+
 use std::vec;
 
+// English characters sorted by frequency
 static ENGLISH_CHARS_BY_FREQ: &'static[u8] = bytes!(
     " eEtTaAiInNoOsSrRlLdDhHcCuUmMfFpPyYgGwWvVbBkKxXjJqQzZ");
 
@@ -24,58 +24,62 @@ struct ByteStat {
     num: uint
 }
 
-// XOR buffer with the key and return the result
+/*
+ * XOR buffer by the key and return the result
+ */
 fn xor_by_key(buffer: &[u8], key: u8) -> ~[u8] {
     buffer.iter().map(|c| c ^ key).collect()
 }
 
-// Return most frequent character from the buffer
+/*
+ * Return most frequent character from the buffer
+ */
 fn get_most_freq_char(buffer: &[u8]) -> u8 {
     if buffer.is_empty() {
         fail!("Buffer is empty");
     }
+    // Statistics map for every possible byte value
     let mut chars = vec::from_fn(256, |i| ByteStat{byte: i as u8, num: 0});
     for &c in buffer.iter() {
         chars[c].num += 1;
     }
-    // Reverse sorting
+    // Sort in reverse order so most frequent character will be the first one
     chars.sort_by(|first, second| second.num.cmp(&first.num));
-    // Most frequent character is the first one now
     chars[0].byte
 }
 
-// Is text in the buffer looks like an English text?
+/*
+ * Is text in the buffer looks like an English text?
+ */
 fn is_english(buffer: &[u8]) -> bool {
-    // TODO: We can't use trigrams or bigrams here because this library
-    // also used to decrypt repeating key XOR in which case it's trying
-    // to guess slices of text.
-    // TODO: Maybe add more allowed characters and tweak percentage
-    let mut allowed = (buffer.len() * 7u) / 100u;
+    /* The check is very simple here, we basically expect only printable
+     * characters. But it should work in most of the cases.
+     *
+     * And we can't use for example n-grams with size greater than 1 here
+     * because this library also used for slices of text.
+     */
     for &c in buffer.iter() {
         match c as char {
             'a'..'z' | 'A'..'Z' | '0'..'9' => (),
-            ' ' | '.' | ',' | '!' | '?' | '\'' | '-' | '"' => (),
-            '\n' | '\r' | '\t' => (),
-            _ => {
-                allowed -= 1u;
-                if allowed <= 0 {
-                    return false;
-                }
-            }
+            ' ' | '.' | ',' | '!' | '?' | '\'' | '-' | '"' | '&' | '%' => (),
+            '#' | '(' | ')' | '\n' | '\r' | '\t' => (),
+            _ => return false
         }
     }
     true
 }
 
-// Try to decrypt encrypted text in the buffer
+/*
+ * Try to decrypt XOR encrypted text in the buffer
+ */
 pub fn decrypt(buffer: &[u8]) -> DecryptionResult {
     if buffer.is_empty() {
         fail!("Buffer is empty");
     }
     let first = get_most_freq_char(buffer);
     for &c in ENGLISH_CHARS_BY_FREQ.iter() {
-        // Most frequent letter in the encrypted text probably should
-        // correspond to the most frequent letter in English
+        // Most frequent characters in the encrypted text probably should
+        // correspond to the most frequent characters in English
         let key = first ^ c;
         let decrypted = xor_by_key(buffer, key);
         if is_english(decrypted) {
@@ -83,4 +87,48 @@ pub fn decrypt(buffer: &[u8]) -> DecryptionResult {
         }
     }
     NotFound
+}
+
+/*
+ * Tests
+ */
+#[cfg(test)]
+mod test {
+    use std::str;
+    use extra::hex::FromHex;
+    use super::{xor_by_key, get_most_freq_char, is_english};
+    use super::{decrypt, Found};
+
+    #[test]
+    fn test_xor_by_key() {
+        let buffer = ~[0xafu8, 0xafu8, 0xafu8, 0xafu8];
+        let key = 0xfau8;
+        assert_eq!(xor_by_key(buffer, key), ~[0x55u8, 0x55u8, 0x55u8, 0x55u8]);
+    }
+
+    #[test]
+    fn test_get_most_freq_char() {
+        let buffer = ~[1u8, 1u8, 2u8, 2u8, 2u8, 3u8, 3u8, 3u8, 3u8];
+        assert_eq!(get_most_freq_char(buffer), 3u8);
+    }
+
+    #[test]
+    fn test_is_english() {
+        assert_eq!(is_english(['t' as u8, 'e' as u8, 's' as u8, 't' as u8]),
+                   true);
+        assert_eq!(is_english([1u8, 2u8, 3u8]), false);
+    }
+
+    #[test]
+    fn test_decrypt() {
+        let buffer = ~"1b37373331363f78151b7f2b783431333d78397828372d363c78\
+                       373e783a393b3736";
+        let (key, decrypted) = match decrypt(buffer.from_hex().unwrap()) {
+            Found(key, decrypted) => (key, decrypted),
+            NotFound => fail!("Key not found")
+        };
+        assert_eq!('X', key as char);
+        assert_eq!("Cooking MC's like a pound of bacon",
+                   str::from_utf8(decrypted));
+    }
 }

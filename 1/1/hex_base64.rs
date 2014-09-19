0e5220d60e434da9f86fbaa832314506226d72b8
Dmitry Vasiliev <dima@hlabs.org>
2014-09-19 20:46:54 +0200

    Update #1 for Rust 0.12

--- 1/1/hex_base64.rs
+++ 1/1/hex_base64.rs
@@ -3,32 +3,32 @@
  * Dmitry Vasiliev <dima@hlabs.org>
  */
 
+extern crate serialize;
+extern crate collections;
 
-extern mod extra;
-
-use extra::hex::{FromHex, ToHex};
-use extra::base64::{STANDARD, FromBase64, ToBase64};
+use serialize::hex::{FromHex, ToHex};
+use serialize::base64::{STANDARD, FromBase64, ToBase64};
 
 
-fn hex_to_bytes(hex: &str) -> ~[u8] {
+fn hex_to_bytes(hex: &str) -> Vec<u8> {
     match hex.from_hex() {
         Ok(bytes) => bytes,
         Err(error) => fail!("Error converting from Hex: {}", error)
     }
 }
 
-fn bytes_to_base64(bytes: &[u8]) -> ~str {
+fn bytes_to_base64(bytes: &[u8]) -> String {
     bytes.to_base64(STANDARD)
 }
 
-fn base64_to_bytes(base64: &str) -> ~[u8] {
+fn base64_to_bytes(base64: &str) -> Vec<u8> {
     match base64.from_base64() {
         Ok(bytes) => bytes,
         Err(error) => fail!("Error converting from Base64: {}", error)
     }
 }
 
-fn bytes_to_hex(bytes: &[u8]) -> ~str {
+fn bytes_to_hex(bytes: &[u8]) -> String {
     bytes.to_hex()
 }
 
@@ -37,18 +37,18 @@ fn bytes_to_hex(bytes: &[u8]) -> ~str {
  */
 #[cfg(not(test))]
 fn main() {
-    use std::str;
+    use collections::str;
 
-    let hex = ~"49276d206b696c6c696e6720796f757220627261696e206c696b65206120\
-                706f69736f6e6f7573206d757368726f6f6d";
+    let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120\
+               706f69736f6e6f7573206d757368726f6f6d";
     println!("Hex     => {}", hex);
     let bytes = hex_to_bytes(hex);
-    println!("Bytes   => {}", str::from_utf8(bytes));
-    let base64 = bytes_to_base64(bytes);
+    println!("Bytes   => {}", str::from_utf8(bytes.as_slice()).unwrap());
+    let base64 = bytes_to_base64(bytes.as_slice());
     println!("Base64  => {}", base64);
-    let bytes2 = base64_to_bytes(base64);
-    println!("Bytes 2 => {}", str::from_utf8(bytes2));
-    let hex2 = bytes_to_hex(bytes2);
+    let bytes2 = base64_to_bytes(base64.as_slice());
+    println!("Bytes 2 => {}", str::from_utf8(bytes2.as_slice()).unwrap());
+    let hex2 = bytes_to_hex(bytes2.as_slice());
     println!("Hex 2   => {}", hex2);
 }
 
@@ -62,11 +62,13 @@ mod test {
 
     #[test]
     fn test_base64_hex() {
-        let hex = ~"49276d206b696c6c696e6720796f757220627261696e206c696b65\
-                    206120706f69736f6e6f7573206d757368726f6f6d";
-        let base64 = ~"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3V\
-                       zIG11c2hyb29t";
-        assert_eq!(bytes_to_base64(hex_to_bytes(hex)), base64);
-        assert_eq!(bytes_to_hex(base64_to_bytes(base64)), hex);
+        let ex_hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65\
+                      206120706f69736f6e6f7573206d757368726f6f6d";
+        let ex_base64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3V\
+                         zIG11c2hyb29t";
+        let base64 = bytes_to_base64(hex_to_bytes(ex_hex).as_slice());
+        let hex = bytes_to_hex(base64_to_bytes(ex_base64).as_slice());
+        assert_eq!(base64.as_slice(), ex_base64);
+        assert_eq!(hex.as_slice(), ex_hex);
     }
 }

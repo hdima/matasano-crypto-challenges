1e7f7f738728f166f3ead54fc7d83d7a069a508d
Dmitry Vasiliev <dima@hlabs.org>
2014-10-02 22:48:51 +0200

    Add enum for AES modes

--- 2/11/aes_oracle.rs
+++ 2/11/aes_oracle.rs
@@ -5,10 +5,34 @@
 
 extern crate aes_lib;
 
-use std::rand::{random, task_rng, Rng};
+use std::rand::{random, task_rng, Rng, Rand};
+use std::fmt::{Show, Formatter, FormatError};
 
 use aes_lib::{AES_BLOCK_SIZE, encrypt_aes_ecb, encrypt_aes_cbc};
 
+
+enum Mode {
+    ECB,
+    CBC
+}
+
+impl Rand for Mode {
+    fn rand<R: Rng>(rng: &mut R) -> Mode {
+        match rng.gen() {
+            true => ECB,
+            false => CBC
+        }
+    }
+}
+
+impl Show for Mode {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), FormatError> {
+        match *self {
+            ECB => f.pad("ECB"),
+            CBC => f.pad("CBC")
+        }
+    }
+}
 
 fn random_bytes(len: uint) -> Vec<u8> {
     Vec::from_fn(len, |_| random::<u8>())
@@ -18,26 +42,21 @@ fn random_uint(low: uint, high: uint) -> uint {
     task_rng().gen_range(low, high)
 }
 
-fn random_bool() -> bool {
-    random::<bool>()
-}
-
-fn aes_encrypt() -> (Vec<u8>, bool) {
+fn aes_encrypt() -> (Vec<u8>, Mode) {
     let key = random_bytes(AES_BLOCK_SIZE);
     let data = random_bytes(AES_BLOCK_SIZE * random_uint(30, 80)
                             + random_uint(0, AES_BLOCK_SIZE));
-    let use_ecb = random_bool();
-    let encrypted = if use_ecb {
-        encrypt_aes_ecb(data.as_slice(), key.as_slice())
-    } else {
-        let iv = random_bytes(AES_BLOCK_SIZE);
-        encrypt_aes_cbc(data.as_slice(), key.as_slice(), iv.as_slice())
-    };
-    (encrypted, use_ecb)
+    match random::<Mode>() {
+        ECB => (encrypt_aes_ecb(data.as_slice(), key.as_slice()), ECB),
+        CBC => {
+            let iv = random_bytes(AES_BLOCK_SIZE);
+            (encrypt_aes_cbc(data.as_slice(), key.as_slice(),
+                             iv.as_slice()), CBC)
+        }
+    }
 }
 
 fn main() {
-    let (encrypted, use_ecb) = aes_encrypt();
-    let mode = if use_ecb {"ECB"} else {"CBC"};
-    println!("Mode: {} Enc: {}", mode, encrypted);
+    let (encrypted, mode) = aes_encrypt();
+    println!("Mode: {}", mode);
 }

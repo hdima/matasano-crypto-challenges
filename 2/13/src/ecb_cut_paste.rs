9a3d6ac5f0f6146884fbb0f28080c8f2cbaa96f6
Dmitry Vasiliev <dima@hlabs.org>
2014-10-23 21:49:01 +0200

    Add profiles with AES encryption

--- 2/13/src/ecb_cut_paste.rs
+++ 2/13/src/ecb_cut_paste.rs
@@ -3,8 +3,50 @@
  * Dmitry Vasiliev <dima@hlabs.org>
  */
 
+extern crate aes_lib;
+
+#[cfg(not(test))]
+use std::rand::random;
 use std::collections::HashMap;
 
+#[cfg(not(test))]
+use aes_lib::{AES_BLOCK_SIZE, decrypt_aes_ecb, encrypt_aes_ecb};
+
+
+#[cfg(not(test))]
+struct Profile {
+    key: Vec<u8>,
+}
+
+#[cfg(not(test))]
+impl Profile {
+    fn new() -> Profile {
+        let key = random_bytes(AES_BLOCK_SIZE);
+        Profile{key: key}
+    }
+
+    fn profile_for(&self, email: &str) -> Vec<u8> {
+        if email.contains("&") || email.contains("=") {
+            fail!("Invalid character in email: {}", email);
+        }
+        let mut map = HashMap::with_capacity(3);
+        map.insert("email".into_string(), email.to_string());
+        map.insert("uid".into_string(), "10".into_string());
+        map.insert("role".into_string(), "user".into_string());
+        let encoded = encode_kv(map);
+        encrypt_aes_ecb(encoded.as_slice(), self.key.as_slice())
+    }
+
+    fn decrypt(&self, encrypted: &[u8]) -> HashMap<String, String> {
+        let encoded = decrypt_aes_ecb(encrypted, self.key.as_slice());
+        parse_kv(encoded.as_slice())
+    }
+}
+
+#[cfg(not(test))]
+fn random_bytes(len: uint) -> Vec<u8> {
+    Vec::from_fn(len, |_| random::<u8>())
+}
 
 fn parse_kv(string: &[u8]) -> HashMap<String, String> {
     let kv = string.split(|&c| c == b'&');
@@ -32,10 +74,9 @@ fn encode_kv(map: HashMap<String, String>) -> Vec<u8> {
 
 #[cfg(not(test))]
 fn main() {
-    let obj = parse_kv(b"foo=bar&baz=qux&zap=zazzle");
-    println!("Obj: {}", obj);
-    let s = encode_kv(obj);
-    println!("String: {}", String::from_utf8_lossy(s.as_slice()));
+    let profile = Profile::new();
+    let encrypted = profile.profile_for("bob@microsoft.com".as_slice());
+    println!("Decrypted: {}", profile.decrypt(encrypted.as_slice()));
 }
 
 /*

9e957d7ef9c0f2d348e32ccef758901a82f1b32a
Dmitry Vasiliev <dima@hlabs.org>
2014-10-23 21:23:27 +0200

    Add tests for parse_kv/encode_kv

--- 2/13/src/ecb_cut_paste.rs
+++ 2/13/src/ecb_cut_paste.rs
@@ -30,9 +30,37 @@ fn encode_kv(map: HashMap<String, String>) -> Vec<u8> {
     bytes
 }
 
+#[cfg(not(test))]
 fn main() {
     let obj = parse_kv(b"foo=bar&baz=qux&zap=zazzle");
     println!("Obj: {}", obj);
     let s = encode_kv(obj);
     println!("String: {}", String::from_utf8_lossy(s.as_slice()));
+}
+
+/*
+ * Tests
+ */
+#[cfg(test)]
+mod test {
+    use std::collections::HashMap;
+    use super::{parse_kv, encode_kv};
+
+    fn split_encoded(string: &[u8]) -> Vec<&[u8]> {
+        let mut pairs: Vec<&[u8]> = string.split(|&c| c == b'&').collect();
+        pairs.sort();
+        pairs
+    }
+
+    #[test]
+    fn test_parse_encode_kv() {
+        let mut map: HashMap<String, String> = HashMap::new();
+        let encoded: Vec<u8> = b"foo=bar&baz=qux&zap=zazzle".to_vec();
+        map.insert("foo".into_string(), "bar".into_string());
+        map.insert("baz".into_string(), "qux".into_string());
+        map.insert("zap".into_string(), "zazzle".into_string());
+        assert_eq!(map, parse_kv(encoded.as_slice()));
+        assert_eq!(split_encoded(encode_kv(map).as_slice()),
+                   split_encoded(encoded.as_slice()));
+    }
 }

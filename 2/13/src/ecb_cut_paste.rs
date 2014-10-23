ecdae79b573a773cc133c209a194742c2fa16adb
Dmitry Vasiliev <dima@hlabs.org>
2014-10-23 22:21:16 +0200

    Replace HashMap with vector of 2-tuples

--- 2/13/src/ecb_cut_paste.rs
+++ 2/13/src/ecb_cut_paste.rs
@@ -5,20 +5,15 @@
 
 extern crate aes_lib;
 
-#[cfg(not(test))]
 use std::rand::random;
-use std::collections::HashMap;
 
-#[cfg(not(test))]
 use aes_lib::{AES_BLOCK_SIZE, decrypt_aes_ecb, encrypt_aes_ecb};
 
 
-#[cfg(not(test))]
 struct Profile {
     key: Vec<u8>,
 }
 
-#[cfg(not(test))]
 impl Profile {
     fn new() -> Profile {
         let key = random_bytes(AES_BLOCK_SIZE);
@@ -29,43 +24,38 @@ impl Profile {
         if email.contains("&") || email.contains("=") {
             fail!("Invalid character in email: {}", email);
         }
-        let mut map = HashMap::with_capacity(3);
-        map.insert("email".into_string(), email.to_string());
-        map.insert("uid".into_string(), "10".into_string());
-        map.insert("role".into_string(), "user".into_string());
+        let map = [("email".into_string(), email.to_string()),
+                   ("uid".into_string(), "10".into_string()),
+                   ("role".into_string(), "user".into_string())].to_vec();
         let encoded = encode_kv(map);
         encrypt_aes_ecb(encoded.as_slice(), self.key.as_slice())
     }
 
-    fn decrypt(&self, encrypted: &[u8]) -> HashMap<String, String> {
+    fn decrypt(&self, encrypted: &[u8]) -> Vec<(String, String)> {
         let encoded = decrypt_aes_ecb(encrypted, self.key.as_slice());
         parse_kv(encoded.as_slice())
     }
 }
 
-#[cfg(not(test))]
 fn random_bytes(len: uint) -> Vec<u8> {
     Vec::from_fn(len, |_| random::<u8>())
 }
 
-fn parse_kv(string: &[u8]) -> HashMap<String, String> {
+fn parse_kv(string: &[u8]) -> Vec<(String, String)> {
     let kv = string.split(|&c| c == b'&');
-    let mut pairs = kv.map(|kv| kv.split(|&c| c == b'='));
-    let mut map = HashMap::new();
-    for mut it in pairs {
-        let pair: Vec<&[u8]> = it.collect();
-        map.insert(String::from_utf8(pair[0].to_vec()).unwrap(),
-                   String::from_utf8(pair[1].to_vec()).unwrap());
-    }
-    map
+    kv.map(|kv| {
+        let pair: Vec<&[u8]> = kv.split(|&c| c == b'=').collect();
+        (String::from_utf8(pair[0].to_vec()).unwrap(),
+         String::from_utf8(pair[1].to_vec()).unwrap())
+        }).collect()
 }
 
-fn encode_kv(map: HashMap<String, String>) -> Vec<u8> {
+fn encode_kv(map: Vec<(String, String)>) -> Vec<u8> {
     let mut bytes = Vec::new();
-    for (k, v) in map.iter() {
-        bytes.push_all(k.clone().into_bytes().as_slice());
+    for item in map.iter() {
+        bytes.push_all(item.ref0().clone().into_bytes().as_slice());
         bytes.push(b'=');
-        bytes.push_all(v.clone().into_bytes().as_slice());
+        bytes.push_all(item.ref1().clone().into_bytes().as_slice());
         bytes.push(b'&');
     }
     bytes.pop();
@@ -84,24 +74,26 @@ fn main() {
  */
 #[cfg(test)]
 mod test {
-    use std::collections::HashMap;
-    use super::{parse_kv, encode_kv};
-
-    fn split_encoded(string: &[u8]) -> Vec<&[u8]> {
-        let mut pairs: Vec<&[u8]> = string.split(|&c| c == b'&').collect();
-        pairs.sort();
-        pairs
-    }
+    use super::{Profile, parse_kv, encode_kv};
 
     #[test]
     fn test_parse_encode_kv() {
-        let mut map: HashMap<String, String> = HashMap::new();
+        let map = [("foo".into_string(), "bar".into_string()),
+                   ("baz".into_string(), "qux".into_string()),
+                   ("zap".into_string(), "zazzle".into_string())].to_vec();
         let encoded: Vec<u8> = b"foo=bar&baz=qux&zap=zazzle".to_vec();
-        map.insert("foo".into_string(), "bar".into_string());
-        map.insert("baz".into_string(), "qux".into_string());
-        map.insert("zap".into_string(), "zazzle".into_string());
         assert_eq!(map, parse_kv(encoded.as_slice()));
-        assert_eq!(split_encoded(encode_kv(map).as_slice()),
-                   split_encoded(encoded.as_slice()));
+        assert_eq!(encode_kv(map), encoded);
+    }
+
+    #[test]
+    fn test_profile() {
+        let profile = Profile::new();
+        let email = "bob@microsoft.com";
+        let encrypted = profile.profile_for(email.as_slice());
+        assert_eq!(profile.decrypt(encrypted.as_slice()),
+                   [("email".into_string(), email.into_string()),
+                    ("uid".into_string(), "10".into_string()),
+                    ("role".into_string(), "user".into_string())].to_vec());
     }
 }

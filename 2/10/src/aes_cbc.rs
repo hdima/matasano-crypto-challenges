7dd8fd9a98b8b8746645cca837aa99f57aa730f9
Dmitry Vasiliev <dima@hlabs.org>
2014-11-22 19:52:53 +0100

    Update code

---
+++ 2/10/src/aes_cbc.rs
@@ -0,0 +1,43 @@
+/* AES-128 CBC mode
+ *
+ * Dmitry Vasiliev <dima@hlabs.org>
+ */
+
+extern crate serialize;
+
+extern crate aes_lib;
+
+use std::path::Path;
+use std::io::fs::File;
+
+use serialize::base64::FromBase64;
+
+use aes_lib::{decrypt_aes_cbc, encrypt_aes_cbc};
+
+fn read_hex_file(path: &Path) -> Vec<u8> {
+    match File::open(path) {
+        Ok(mut file) => {
+            let data = file.read_to_end().unwrap();
+            let text = String::from_utf8(data).unwrap();
+            text.from_base64().unwrap()
+        },
+        Err(err) => panic!("Unable to open {}: {}", path.display(), err)
+    }
+}
+
+/*
+ * Main entry point
+ */
+fn main() {
+    let path = Path::new("10.txt");
+    let data = read_hex_file(&path);
+    let key = b"YELLOW SUBMARINE";
+    let iv = Vec::from_elem(16u, 0u8);
+    let decrypted = decrypt_aes_cbc(data.as_slice(), key.as_slice(),
+                                    iv.as_slice());
+    println!("Decrypted => \"{}\"",
+             String::from_utf8(decrypted.clone()).unwrap());
+    assert_eq!(data, encrypt_aes_cbc(decrypted.as_slice(), key.as_slice(),
+                                     iv.as_slice()));
+    println!("Encryption OK!");
+}
